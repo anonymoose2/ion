1ec2bd79fd9b38ca4bf0cafcc87d88498d7e34b7
William Zhang <2018wzhang@tjhsst.edu>
2018-04-01 00:53:42 -0400

    fix thing once and for all

--- intranet/static/js/bus.js
+++ intranet/static/js/bus.js
@@ -470,7 +470,9 @@ $(function() {
             if (!this.busDriverBus) {
                 return;
             }
-            if (e.keyCode === 38 || e.keyCode === 87) {
+            if (e.keyCode === 37 || e.keyCode === 65 ||
+                e.keyCode === 39 || e.keyCode === 68 ||
+                e.keyCode === 38 || e.keyCode === 87) {
                 this.busDriverBus.pressed = false;
             }
         },
